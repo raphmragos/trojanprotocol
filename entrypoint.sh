4d35d1114e78dcfc5501aeb94bf4f88bedc656b8
raphmragos <raphmragos@gmail.com>
2026-07-14 12:26:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-
-# Simulan ang Xray sa likod
 /usr/local/bin/xray/xray -config /etc/xray/config.json &
-
-# Simulan ang Nginx sa harap (pang-maintain ng container na bukas)
 nginx -g 'daemon off;'
