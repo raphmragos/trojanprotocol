3ff6ec22064ff3b5fd5521def0da2bace3092ddb
raphmragos <raphmragos@gmail.com>
2026-07-14 14:06:45 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 set -e
 
-# I-set ang port para sa Nginx
-export PORT=${PORT:-8080}
-sed -i "s/\${PORT:-8080}/$PORT/g" /usr/local/openresty/nginx/conf/nginx.conf
-
 # Simulan ang Xray sa likod
 /usr/local/bin/xray run -c /etc/xray.json &
 
-# Simulan ang OpenResty (naka-foreground)
+# Hintayin nang kaunti para masiguradong nakabukas na ang Xray
+sleep 2
+
+# Simulan ang OpenResty sa harap
 exec /usr/local/openresty/bin/openresty -g 'daemon off;'
