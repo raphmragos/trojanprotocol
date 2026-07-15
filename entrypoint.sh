fc621e74b840ac8d1c4d942d90e71fd7bf68327e
raphmragos <raphmragos@gmail.com>
2026-07-15 14:23:13 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+set -e
+
+# Simulan ang Xray sa background
 xray run -c /etc/xray.json &
+
+# Maghintay ng 2 segundo para masiguradong handa na ang Xray
+sleep 2
+
+# Simulan ang Nginx sa foreground
 exec nginx -g 'daemon off;'
