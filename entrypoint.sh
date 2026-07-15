97e5f11f44fc1437f5df0bcec0a0cae5a9b69163
raphmragos <raphmragos@gmail.com>
2026-07-15 15:16:52 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,1 @@
-#!/bin/sh
-set -e
 
-# Simulan ang Xray
-xray run -c /etc/xray.json &
-
-# Maghintay ng 3 segundo
-sleep 3
-
-# I-test muna kung gumagana ang Nginx bago tumakbo
-nginx -t
-
-# Simulan ang Nginx
-exec nginx -g 'daemon off;'
