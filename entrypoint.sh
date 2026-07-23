b52f3d80b3788b1071e54c1ab8239ed19b28c163
raphmragos <raphmragos@gmail.com>
2026-07-23 20:03:48 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+set -e
 
-# Simulan ang Xray sa background
-echo "Nagsisimula ng Xray..."
+# Simulan muna ang Xray sa background
+echo "🔧 Sinisimulan ang Xray..."
 /usr/local/bin/xray run -c /etc/xray.json &
 
-# Simulan ang OpenResty/Nginx sa foreground
-echo "Nagsisimula ng OpenResty..."
-exec /usr/local/openresty/bin/openresty -g 'daemon off;'
+# I-set ang port ng Nginx base sa Cloud Run
+export NGINX_PORT=${PORT:-8080}
+sed -i "s/\${PORT:-8080}/$NGINX_PORT/g" /usr/local/openresty/nginx/conf/nginx.conf
 
+# Simulan ang OpenResty/Nginx
+echo "🌐 Sinisimulan ang OpenResty sa port $NGINX_PORT..."
+exec /usr/local/openresty/bin/openresty -g 'daemon off;'
