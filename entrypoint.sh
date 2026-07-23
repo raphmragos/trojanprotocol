98ba1b73699b37e211bbc0bfd1ac7112cff1b7d4
raphmragos <raphmragos@gmail.com>
2026-07-23 19:51:03 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
+
+# Simulan ang Xray sa background
+echo "Nagsisimula ng Xray..."
+/usr/local/bin/xray run -c /etc/xray.json &
+
+# Simulan ang OpenResty/Nginx sa foreground
+echo "Nagsisimula ng OpenResty..."
+exec /usr/local/openresty/bin/openresty -g 'daemon off;'
 
